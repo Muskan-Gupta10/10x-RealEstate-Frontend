eb265adb322fa749eaf6da9416c016631b731aca
mrloobo <mahajandhiraj99@gmail.com>
2023-06-09 14:28:04 +0530

    Navbar Created

---
+++ real-estate-app/src/Components/Navbar.jsx
@@ -0,0 +1,60 @@
+import React from "react";
+import "../Styles/navbar.css";
+import property_image from "../Images/Navbar_Images/property.png";
+import assistance_image from "../Images/Navbar_Images/assistance.png"
+import received_image from "../Images/Navbar_Images/received.png";
+import sent_image from "../Images/Navbar_Images/sent.png";
+import views_image from "../Images/Navbar_Images/eye.png";
+import tarrif_image from "../Images/Navbar_Images/tarrifplan.png";
+
+export default function Navbar() {
+  return (
+    <>
+      <div className="navbar-part">
+        <div className="logo">
+            <h1>LOGO</h1>
+        </div>
+        <div>
+          <ul class="nav flex-column">
+            <li class="nav-item navbar-element">
+              <img src={property_image} alt="not found" className="ele-images"/>
+              <a class="nav-link active" href="/">
+                Property
+              </a>
+            </li>
+            <li class="nav-item">
+              <img src={assistance_image} alt="not found" className="ele-images"/>
+              <a class="nav-link" href="/">
+                Assistance
+              </a>
+            </li>
+            <li class="nav-item">
+              <img src={received_image} alt="not found" className="ele-images"/>  
+              <a class="nav-link" href="/">
+                Received Interest
+              </a>
+            </li>
+            <li class="nav-item">
+              <img src={sent_image} alt="not found" className="ele-images"/>
+              <a class="nav-link" href="/">
+                Sent Interest
+              </a>
+            </li>
+            <li class="nav-item">
+              <img src={views_image} alt="not found" className="ele-images"/>
+              <a class="nav-link" href="/">
+                Property Views
+              </a>
+            </li>
+            <li class="nav-item">
+              <img src={tarrif_image} alt="not found" className="ele-images"/>
+              <a class="nav-link" href="/">
+                Tarrif Plan
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </>
+  );
+}
