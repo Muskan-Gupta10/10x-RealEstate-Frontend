292ca308bd74e0402d9df47d4f2c1608693b372b
Mus2000kan <muskangupta1018@gmail.com>
2023-06-09 15:39:36 +0530

    added sub header

---
+++ real-estate-app/src/Components/InfoHeader.js
@@ -0,0 +1,25 @@
+import React from 'react'
+//import "./InfoHeader.css"
+//import { NavLink } from 'react-router-dom'
+function InfoHeader() {
+  return (
+    <div className='navbar'>
+        <div className='navheading'>
+            
+            <h1>Add New Property</h1>
+        </div>
+        <div className='sections'>
+          <span>Basic Info</span>
+          <span>Property Detail</span>
+          <span>General Info</span>
+          <span>Location Info</span>
+       
+        </div>
+      
+        </div>
+      
+
+  )
+}
+
+export default InfoHeader
