e7a294a6c370f048e8fd577d3cde9b461a6167fa
mrloobo <mahajandhiraj99@gmail.com>
2023-06-10 20:39:37 +0530

    Auth Folder not found

--- real-estate-app/src/App.js
+++ real-estate-app/src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import Navbar from "../src/Components/Navbar";
 import PropertyView from "../src/Components/PropertyView"
 import Header from './Components/Header';
+import Generalinfo from './Components/Generalinfo';
 
 function App() {
   return <>
