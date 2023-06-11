27aa2c69656079011f913f218e7af3e6e1007416
Mus2000kan <muskangupta1018@gmail.com>
2023-06-11 18:05:49 +0530

    updated basicinfo and propdetails,added Generalinfo and LocationInfo

--- real-estate-app/src/App.js
+++ real-estate-app/src/App.js
@@ -1,5 +1,7 @@
 import PropertyDetails from './Components/PropertyDetails'
 import PropertyView from './Components/PropertyView'
+import Generalinfo from "./Components/Generalinfo"
+
 
 export default function App(){
   return(
